7646c2eccf89bf0fb254d75c7250f9c1c15904f6
shreetishrestha <shreeti.shrestha@cotiviti.com>
2021-04-06 14:26:51 +0545

    Standup updated

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ Vue.component('team-intro', {
 Vue.component('team-updates', {
     props: ['updates'],
 
-template: '<table><thead><tr><th>Date</th><th v-for="member in listOfMembers">{{member}}</th></tr></thead><tbody><tr v-for="update, date in updates" :key="date"><td>{{date}}</td><td v-for="memberUpdate, member in update" :key="member"><ul><li v-for="task in memberUpdate" :key="task.id"> {{task.title}}</li></ul></td></tr></tbody></table>',
+    template: '<table><thead><tr><th>Date</th><th v-for="member in listOfMembers">{{member}}</th></tr></thead><tbody><tr v-for="update, date in updates" :key="date"><td>{{date}}</td><td v-for="memberUpdate, member in update" :key="member"><ul><li v-for="task in memberUpdate" :key="task.id"> {{task.title}}</li></ul></td></tr></tbody></table>',
     computed: {
         listOfDates() {
             console.info(this.updates)
-            return Object.keys(this.updates) 
+            return Object.keys(this.updates)
         },
         listOfMembers() {
             const first = Object.keys(this.updates)[0]
@@ -20,9 +20,29 @@ template: '<table><thead><tr><th>Date</th><th v-for="member in listOfMembers">{{
     }
 })
 
+Vue.component('new-update', {
+    props: ['updates', 'members'],
+    data() {
+        return {
+            newUpdate: {
+                title: null,
+                member: 'Shreeti',
+            }
+        }
+    },
+    methods: {
+        submitUpdate: function () {
+            this.$emit('update-updates', this.newUpdate)
+        }
+    },
+    template: '<div><label for="title">Title</label><input type="text" v-model.text="newUpdate.title"><br/><label for="member">Member</label><select v-model="newUpdate.member"><option v-for="member in members" :value="member">{{member}}</option></select><br/><span>Selected = {{newUpdate.member}}</span><br/><button @click="submitUpdate">Submit</button></div>'
+
+})
+
 const app = new Vue({
     el: '#app',
     data: {
+        errors: [],
         teamInfo: {
             name: 'Rudder',
             members: [
@@ -30,7 +50,7 @@ const app = new Vue({
             ]
         },
         updates: {
-            '5/4/2021': {
+            '4/5/2021': {
                 'Shreeti': [
                     { id: 0, title: 'To learn Vue JS', complete: false },
                     { id: 1, title: 'To learn Leaflet', complete: false }
@@ -64,6 +84,31 @@ const app = new Vue({
                     { id: 0, title: 'Test! Test and Repeat', complete: false },
                     { id: 1, title: 'To learn D3', complete: false }
                 ]
+            }
+        },
+    }, methods: {
+        updateUpdates: function (newUpdate) {
+            const member = newUpdate.member
+            const title = newUpdate.title
+            const date = (new Date()).toLocaleString().split(',')[0]
+            const currentUpdates = { ...this.updates }
+            if (this.updates[date]) {
+                currentUpdates[date][member].push({
+                    id: currentUpdates[date][member].length ? currentUpdates[date][member].length + 1 : 0,
+                    title: title,
+                })
+                this.updates = { ...currentUpdates }
+            } else {
+                currentUpdates[date] = {}
+                this.teamInfo.members.forEach(mem => {
+                    currentUpdates[date][mem] = []
+                })
+                currentUpdates[date][member].push({
+                    id: 0,
+                    title: title,
+                })
+                this.updates = { ...this.updates, ...currentUpdates }
+
             }
         }
     }
